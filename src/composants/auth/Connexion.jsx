9bb97ac4c004ac12e44b819cf9a8dd98fc849063
willbreezy <wilfried11ndong@gmail.com>
2024-08-04 21:01:08 +0100

    Connexion et inscription utilisateur ok

--- src/composants/auth/Connexion.jsx
+++ src/composants/auth/Connexion.jsx
@@ -1,40 +1,49 @@
-import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { login } from '../../actions/authentifier-utilisateur';
+/**
+ * Ce composant React utilise Redux pour gérer l'état d'authentification.
+ * Il permet à un utilisateur de se connecter en soumettant  un formulaire avec son adresse mail et 
+ * son mot de passe. Dans la mesure où l'utilisateur est authentifié il est redirigé vers le tableau de bord. 
+ */
 
-const Login = ({ login, isAuthenticated }) => {
-  const [formData, setFormData] = useState({
+import React, { useState } from 'react'; // useState pour gérer l'état local du formulaire
+import { Link, Navigate } from 'react-router-dom'; // pour gérer la navigation
+import { connect } from 'react-redux'; // pour connecter le composant à Redux
+import PropTypes from 'prop-types'; // pour valider les props
+import { login } from '../../actions/authentifier-utilisateur'; // pour l'action de connexion
+
+const Connexion = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({ // formData est l'état local qui contient email et password
     email: '',
     password: ''
   });
 
   const { email, password } = formData;
 
+  // onChange met à jour l'état local formData lorsque l'utilisateur entre des valeurs dans les champs du formulaire
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  // onSubmit empeche le comportement par defaut du formulaire et on appelle l'action 'login' avec les valeurs 'email' et 'password'
   const onSubmit = (e) => {
     e.preventDefault();
     login(email, password);
   };
 
+  // si l'utilisateur est identifié, il est redirigé vers le tableau de bord /dashboard à l'aide  de 'Navigate'
   if (isAuthenticated) {
     return <Navigate to="/dashboard" />;
   }
 
   return (
     <section className="container">
-      <h1 className="large text-primary">Sign In</h1>
+      <h1 className="large text-primary">Se connecter</h1>
       <p className="lead">
-        <i className="fas fa-user" /> Sign Into Your Account
+        <i className="fas fa-user" /> Connectez-vous à votre compte DevConnect
       </p>
       <form className="form" onSubmit={onSubmit}>
         <div className="form-group">
           <input
             type="email"
-            placeholder="Email Address"
+            placeholder="Salut, votre adresse email"
             name="email"
             value={email}
             onChange={onChange}
@@ -43,23 +52,23 @@ const Login = ({ login, isAuthenticated }) => {
         <div className="form-group">
           <input
             type="password"
-            placeholder="Password"
+            placeholder="Et votre mot de passe"
             name="password"
             value={password}
             onChange={onChange}
             minLength="6"
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Login" />
+        <input type="submit" className="btn btn-primary" value="Se connecter" />
       </form>
       <p className="my-1">
-        Don't have an account? <Link to="/register">Sign Up</Link>
+        Vous n'avez pas de compte? <Link to="/register">C'est simple, inscrivez-vous!</Link>
       </p>
     </section>
   );
 };
 
-Login.propTypes = {
+Connexion.propTypes = {
   login: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool
 };
@@ -68,4 +77,4 @@ const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(mapStateToProps, { login })(Login);
+export default connect(mapStateToProps, { login })(Connexion);
---
+++ src/composants/auth/Inscription.jsx
@@ -0,0 +1,108 @@
+/**
+ * Ce composant d'inscription d'utilisateur permet de gérer l'état de l'authentification et les alertes.
+ * Il permet à un utilisateur de créer un compte en soumettant un formulaire avec son nom et prénom, 
+ * son adresse email et son mot de passe. Si les mots de passe ne corrspondent pas, une alerte est affichée. 
+ * Si l'utilisateur est authentifié, il est redirigé vers le tableau de bord. 
+ */
+
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, Navigate } from 'react-router-dom';
+import { setAlert } from '../../actions/alertes';
+import { register } from '../../actions/alertes';
+import PropTypes from 'prop-types';
+
+const Inscription = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  // ici, onSubmit va aussi s'assurer que les mots de passe correspondent
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert('Oulah, vos mots de passe ne correspondent pas', 'réessayez encore');
+    } else {
+      register({ name, email, password });
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Inscrivez-vous</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Créez votre compte
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Saisissez votre nom et votre prénom"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Ainsi que votre adresse email"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            Ce site est magnifique, n'est-ce pas, et si vous insériez votre plus belle 
+            photo !?
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Et votre mot de passe je vous prie"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirmez votre mot de passe ici"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="S'inscrire" />
+      </form>
+      <p className="my-1">
+        Avez-vous déjà un compte DevConnect? <Link to="/login">Super, connectez-vous ici</Link>
+      </p>
+    </section>
+  );
+};
+
+Inscription.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Inscription);
